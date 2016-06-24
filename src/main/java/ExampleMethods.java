38e51da204ca077825efa3e901611f2d680419db
Ruben Jongejan <ruben.jongejan@gmail.com>
2016-06-24 09:45:34 +0200

    First commit

---
+++ src/main/java/ExampleMethods.java
@@ -0,0 +1,21 @@
+/**
+ * Created by ruben on 24-6-2016.
+ */
+public class ExampleMethods {
+
+    public int plusFive(int i){
+        return i + 5;
+    }
+
+    public String stringToStling(String string){
+        return string.replaceAll("r", "l");
+    }
+
+    public String toUppercase(String input){
+        return input.toUpperCase();
+    }
+
+    public String stitchTogether(String first, String second){
+        return first + second;
+    }
+}
---
+++ src/test/java/ExampleMethodsTest.java
@@ -0,0 +1,40 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ruben on 24-6-2016.
+ */
+public class ExampleMethodsTest {
+    @BeforeClass
+    public static void startTests(){
+        System.out.println("Tests started! :)");
+    }
+
+    @Test
+    public void plusFive() {
+
+    }
+
+    @Test
+    public void stringToStling() {
+
+    }
+
+    @Test
+    public void toUppercase() {
+
+    }
+
+    @Test
+    public void stitchTogether() {
+
+    }
+
+    @AfterClass
+    public static void afterTests(){
+        System.out.println("Tests finished!");
+    }
+}
